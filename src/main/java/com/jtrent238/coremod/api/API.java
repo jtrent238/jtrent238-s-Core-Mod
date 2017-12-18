6eeafbd3bd156823b9cbd2214fb558ac503f52bd
Trent Patterson <jtrent238@outlook.com>
2017-12-18 15:06:13 -0500

    Add files via upload

---
+++ src/main/java/com/jtrent238/coremod/api/API.java
@@ -0,0 +1,15 @@
+package com.jtrent238.coremod.api;
+
+public @interface API {
+
+	String owner();
+
+	String provides();
+
+	String apiVersion();
+
+	String modName();
+	
+	String modID();
+}
+
---
+++ src/main/java/com/jtrent238/coremod/api/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @author jtrent238
+ *
+ */
+@API(owner = "jtrent238", provides = "jtrent238CoreModAPI", apiVersion = com.jtrent238.coremod.APIVERSION, modName = "", modID = "")
+package com.jtrent238.coremod.api;
+
